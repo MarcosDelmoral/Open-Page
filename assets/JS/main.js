37dcdd8fb3572b59f85e999a6e84914029f822bd
Marcos Delmoral <Marcos_delmoral@outlook.com>
2023-05-10 17:07:41 -0400

    back to top

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -11,4 +11,30 @@ function vista(elemento) {
     }, 380);
 };
 
+
+
+// Get the button:
+let mybutton = document.getElementById("btntop");
+    window.onscroll = function() {scrollFunction()};
+    
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.classList.remove('d-none');
+        mybutton.classList.add('d-block');
+        console.log('si')
+      } else {
+        mybutton.classList.remove('d-block');
+        mybutton.classList.add('d-none');
+        console.log('no')
+      }
+    }
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
 main();
