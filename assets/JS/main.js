7f42bd20c8ea2b9631702a7e72c01d4a5460c799
Marcos Delmoral <Marcos_delmoral@outlook.com>
2023-05-09 22:45:59 -0400

    animation onload

---
+++ assets/JS/main.js
@@ -0,0 +1,5 @@
+console.log('si');
+window.addEventListener('load',() => {
+    document.getElementById('principal').classList.add('d-block','animate__animated', 'animate__fadeInLeft');
+    document.getElementById('principal').classList.remove('d-none');
+})
